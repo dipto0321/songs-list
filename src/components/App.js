007b6b9f5aed9e693538b5c9ec29e1a28950007a
Dipto Karmakar <diptokmk47@gmail.com>
2019-08-08 12:08:26 +0600

    Render static songs as a list

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import SongList from './SongList';
+import appStyles from '../css/App.module.css';
 
 const App = () => (
-  <div className="app">
+  <div className={`${appStyles.mt} ui container`}>
     <SongList />
   </div>
 );
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import songListStyles from '../css/SongList.module.css';
 
 class SongList extends Component {
-  state = {};
+  renderList = ({ songs }) => {
+    return songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  };
 
   render() {
-    return <div className="song-list">SongList</div>;
+    return (
+      <div className={`${songListStyles.mt}  ui relaxed divided list`}>
+        {this.renderList(this.props)}
+      </div>
+    );
   }
 }
 
